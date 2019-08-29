a4af2de51bbde72d6b6eb22ca43f257a984b9f30
MikuSugar <syfangjie@live.cn>
2019-08-29 15:52:28 +0800

    “细节完善”

--- src/main/java/cn/mikusugar/dynamic/Dynamic.java
+++ src/main/java/cn/mikusugar/dynamic/Dynamic.java
@@ -3,7 +3,7 @@ package cn.mikusugar.dynamic;
 import java.io.OutputStream;
 import java.util.Map;
 
-//动态服务接口实现类
+//动态服务接口类
 public interface Dynamic {
     boolean respone(Map<String,String> requestMap, Map<String,String> values, OutputStream out);
 }
